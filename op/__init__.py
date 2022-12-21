5cd5d6bbd45d7e94938939fe175477208b1a226d
B.H.J Thate <thatebhj@gmail.com>
2022-12-21 15:23:34 +0100

    add op package

---
+++ op/__init__.py
@@ -0,0 +1,14 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0622
+
+
+"object programming (op)"
+
+
+from op import hdl, obj, run, thr
+
+
+from op.hdl import *
+from op.obj import *
+from op.run import *
+from op.thr import *
